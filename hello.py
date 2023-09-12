2a9bd90c02842c5b6561be5a6bebdd22664f9616
SANJAYTHURAGA <118811600+SANJAYTHURAGA@users.noreply.github.com>
2023-09-12 17:00:58 +0530

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Netflix Clone</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="netflix-logo.png" alt="Netflix Logo">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">TV Shows</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">My List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="hero">
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <p>Watch anywhere. Cancel anytime.</p>
+            <button>Watch Free for 30 Days</button>
+        </div>
+    </header>
+
+    <!-- Movie Section -->
+    <section class="movies">
+        <!-- Add movie posters and details here -->
+    </section>
+
+    <!-- Footer Section -->
+    <footer>
+        <p>&copy; 2023 Netflix Clone</p>
+    </footer>
+</body>
+</html>
